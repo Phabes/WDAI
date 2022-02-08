3d1431f9b0fca16a95dae5bf784366f9f0cf32d7
McWojownik <49338133+McWojownik@users.noreply.github.com>
2022-02-08 17:54:09 +0100

    lab5 finished angular_backend

--- zad5/src/environments/environment.ts
+++ zad5/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAggv7W6LumBcK0mAUgoRLYt6wcx2b_CkU",
+    authDomain: "student-wdai.firebaseapp.com",
+    projectId: "student-wdai",
+    storageBucket: "student-wdai.appspot.com",
+    messagingSenderId: "999390391361",
+    appId: "1:999390391361:web:0222444d0eaf800c376ac0",
+    measurementId: "G-TY70J3B4T1",
+  },
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import "zone.js/plugins/zone-error"; // Included with Angular CLI.
